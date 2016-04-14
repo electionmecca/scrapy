3414177e466d9d1bc00acf2b2255a084939e480a
Election Mecca <every.man.a.king@gmail.com>
2016-04-14 06:27:04 -0500

    first commit

---
+++ oddschecker/oddschecker/items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class OddscheckerItem(scrapy.Item):
+    # define the fields for your item here like:
+    # number = scrapy.Field()
+    name = scrapy.Field()
+    # trainer = scrapy.Field()
+    pass
